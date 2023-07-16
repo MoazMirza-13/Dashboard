580a63798e765415e868b43acbe8d8262fdf356d
MoazMirza-13 <moazmirza13@gmail.com>
2023-07-16 14:38:16 +0500

    Fixed .

--- src/httpService/Service.js
+++ src/httpService/Service.js
@@ -3,9 +3,9 @@ import axios from "axios";
 // graphChartData
 export const graphChart = async () => {
   const url1 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=20&max=100&count=6";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=20&max=100&count=6";
   const url2 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=20&max=100&count=6";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=20&max=100&count=6";
 
   try {
     const response1 = await axios.get(url1);
@@ -22,11 +22,11 @@ export const graphChart = async () => {
 // barChartData
 export const barChart = async () => {
   const url1 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=150&max=380&count=9";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=150&max=380&count=9";
   const url2 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=150&max=300&count=9";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=150&max=300&count=9";
   const url3 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=150&max=350&count=9";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=150&max=350&count=9";
 
   try {
     const response1 = await axios.get(url1);
@@ -44,9 +44,9 @@ export const barChart = async () => {
 // dailyTrrafficData
 export const dailyTrafficChart = async () => {
   const url1 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=15&max=60&count=7";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=15&max=60&count=7";
   const url2 =
-    "http://www.randomnumberapi.com/api/v1.0/random?min=2&max=10&count=1";
+    "https://www.randomnumberapi.com/api/v1.0/random?min=2&max=10&count=1";
   try {
     const response1 = await axios.get(url1);
     const response2 = await axios.get(url2);
